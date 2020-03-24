9e1cffc7e1952d072155e0f24be951e15a14a16a
raparicio6 <rodrigoaparicio6@gmail.com>
2020-03-24 06:05:14 -0300

    refactor in footer size

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -6,7 +6,7 @@ import styles from './styles.module.scss';
 function Footer({ className }) {
   return (
     <div className={`${className} ${styles.container} row middle`}>
-      <span>Hecho por Rodrigo Aparicio</span>
+      <span className={styles.information}>Hecho por Rodrigo Aparicio</span>
       <a className={styles.repoLink} href="https://github.com/raparicio6/pokemon-finder-react">
         Link a mi repo
       </a>
